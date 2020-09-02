8cf1d12cf9a93b4ef894a8d5c70535b97772a55f
ANUJ JAIN <jainanuj827@gmail.com>
2020-09-02 23:31:52 +0530

    Add files via upload

---
+++ 05_Loops/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _05_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            while (counter < 10)
+            {
+                Console.WriteLine($"Hello World! The counter is {counter}");
+                counter++;
+            }
+
+            first();
+            second();
+
+            
+        }
+
+        static void first()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("--------------Inside First---------");
+
+
+            for (int counter = 0; counter < 10; counter++)
+            {
+                Console.WriteLine($"Hello World! The counter is {counter}");
+            }
+        }
+
+        static void second()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("--------------Inside Second---------");
+            Console.WriteLine("--------------Branches and loops combined---------");
+
+
+            int sum = 0;
+            for (int number = 1; number < 21; number++)
+            {
+                if (number % 3 == 0)
+                {
+                    sum = sum + number;
+                }
+            }
+            Console.WriteLine($"The sum is {sum}");
+        }
+    }
+}
---
+++ 06_Collection/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06_Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string> { "<ANUJ>", "Ana", "Felipe" };
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+
+            Console.WriteLine();
+            names.Add("Maria");
+            names.Add("Bill");
+            names.Remove("Ana");
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+
+            Console.WriteLine($"My name is {names[0]}.");
+            Console.WriteLine($"I've added {names[2]} and {names[3]} to the list.");
+
+            Console.WriteLine($"The list has {names.Count} people in it");
+
+            Console.WriteLine();
+            Console.WriteLine("Search and sort list");
+            var index = names.IndexOf("Felipe");
+            if (index != -1)
+                Console.WriteLine($"The name {names[index]} is at index {index}");
+
+            var notFound = names.IndexOf("Not Found");
+            Console.WriteLine($"When an item is not found, IndexOf returns {notFound}");
+
+            names.Sort();
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+
+            first();
+        }
+
+        static void first()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("List of other types");
+
+            var fibonacciNumbers = new List<int> { 1, 1 };
+            var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+            var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+            fibonacciNumbers.Add(previous + previous2);
+
+            foreach (var item in fibonacciNumbers)
+                Console.WriteLine(item);
+
+        }
+    }
+}
